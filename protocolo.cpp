52dbd18e446938688b164a3f21d84d4babc95f53
alopezj97 <alopezj97@gmail.com>
2018-10-03 12:55:39 +0200

    primera modificacion hello world

--- protocolo.cpp
+++ protocolo.cpp
@@ -7,7 +7,13 @@ int main(){
 
   bool chocobollo=true;
 
-  
+  cout << "Como primer ejemplo, vamos a hacer el Hello World!" << endl;
+
+  cout << "Hello World" << endl;
+
+  // Hemos usado una función incluida en la biblioteca iostream y usado el namespace std
+
+
 
 
 
