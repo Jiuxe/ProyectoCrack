45754de9ad68cdd7605906bd27c761a50a261a77
alopezj97 <alopezj97@gmail.com>
2018-10-03 12:58:51 +0200

    segunda modificacion tipos basicos

--- protocolo.cpp
+++ protocolo.cpp
@@ -13,6 +13,15 @@ int main(){
 
   // Hemos usado una función incluida en la biblioteca iostream y usado el namespace std
 
+  // A continuación definiremos una variable para cada tipo básico de dato
+
+  int numero = 7 ;
+  cout << "numero = " << numero << endl;
+
+  char caracter = 'C';
+  cout << "caracter = " << caracter << endl ;
+  
+
 
 
 
